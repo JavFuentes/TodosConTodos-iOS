c275f70619de67b12a49d6b4eab4c67edf0b6434
JavFuentes <javier.antonio8128@gmail.com>
2024-01-23 23:06:07 -0300

    Initial Commit

---
+++ TodosConTodos/TodosConTodosApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodosConTodosApp.swift
+//  TodosConTodos
+//
+//  Created by Javier Fuentes on 23-01-24.
+//
+
+import SwiftUI
+
+@main
+struct TodosConTodosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
